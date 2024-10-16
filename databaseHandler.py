b6cad4debe1c7fd46879908a7ee086a6e3950d17
Peder17Personal <102620965+Peder17Personal@users.noreply.github.com>
2024-10-16 15:11:58 +0200

    Update databaseHandler.py

--- databaseHandler.py
+++ databaseHandler.py
@@ -2,6 +2,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import datetime
 
 # Replace with your actual service account key file path
 cred = credentials.Certificate("C:/Users/LassePedersen/Documents/GitHub/RatClock/ratclock-59c8f-firebase-adminsdk-gsxkf-be6f53df8c.json")
@@ -14,32 +15,33 @@ firebase_admin.initialize_app(cred, {
 })
 
 # Reference to your Realtime Database
-ref = db.reference('/')
-
-
+database = db.reference('/')
 
 def read(searchElement = 'NO'):
     #We read all the data and then sort/filter. Should be changed for effeciency and speed. 
     
-    
-    data = ref.get()
-    
-    print(len(data))
+    data = database.get()
+    print(data)
 
-    if(len(searchElement) == 0 ):
-        print("SearchElement empty")
+    
+    if(searchElement == 'NO'):
+        data = database.get()
+    
+        print(len(data))
+
+        if(len(data) == 0 ):
+            print("SearchElement empty")
     else:
         print("Not Empty")
     
     return data
 
 def write(DB = 'AlarmTime', data_name = 'tomorrow', data_value = 'NAN'):
-    ref.child(DB).set({ data_name : data_value })
+    database.child(DB).set({ data_name : data_value })
+
 
     
 
 
-
-
-read()
+read('YES')
 
