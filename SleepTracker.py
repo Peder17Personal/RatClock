9befcf11d95299700548db200f037730f43a2ad8
Peder17Personal <102620965+Peder17Personal@users.noreply.github.com>
2024-12-03 15:38:28 +0100

    Create SleepTracker.py

---
+++ SleepTracker.py
@@ -0,0 +1,26 @@
+#Purpose: To generate daily/weekly reports on the sleep pattern. 
+#Function 1: Write what time the sleepsensor has been active
+
+import os
+import shutil
+import numpy as np
+import pandas as pd
+import calendar
+from datetime import datetime
+from fpdf import FPDF
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['axes.spines.top'] = False
+rcParams['axes.spines.right'] = False
+
+
+def sleepTrackerRaporter(jsonData, day):
+    print("10")
+
+
+def main():
+    print("Hello World!")
+
+    if __name__ == "__main__":
+        main() 
